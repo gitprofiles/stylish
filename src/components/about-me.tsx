69abc7ff361e4be624298327160d23dbc2961d23
veloii <85405932+veloii@users.noreply.github.com>
2023-10-14 16:08:21 +0100

    fix about me

--- src/components/about-me.tsx
+++ src/components/about-me.tsx
@@ -1,21 +1,25 @@
-export default function AboutMe({ children }: { children: React.ReactNode }) {
+export default function AboutMe(
+  { children, imageUrl }: { children: React.ReactNode; imageUrl?: string },
+) {
   return (
-    <div className="flex justify-between h-full items-center lg:items-start gap-16 flex-col lg:flex-row">
+    <div className="flex justify-between h-full gap-16 flex-col lg:flex-row">
       <div className="space-y-4">
         <h3>About me</h3>
 
-        <div className="text-lg dark:text-white text-justify space-y-6">
+        <div className="text-lg dark:text-white text-justify space-y-6 opacity-75">
           {children}
         </div>
       </div>
 
-      <div className="flex-shrink-0 flex-grow h-full lg:w-[38%] md:w-3/4 w-full">
-        <img
-          src="https://picsum.photos/600/600"
-          alt="Profile picture"
-          className="rounded-lg object-cover h-full w-full max-h-[300px]"
-        />
-      </div>
+      {imageUrl && (
+        <div className="flex-grow max-w-sm lg:w-[1200px]">
+          <img
+            src="https://picsum.photos/600/600"
+            alt="Profile picture"
+            className="rounded-lg object-cover w-full h-full"
+          />
+        </div>
+      )}
     </div>
   );
 }
