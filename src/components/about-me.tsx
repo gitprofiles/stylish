8c0b33d480f024d0aaad69fc6b7e5961048f7b8f
veloii <85405932+veloii@users.noreply.github.com>
2023-10-14 16:40:18 +0100

    fix about me again

--- src/components/about-me.tsx
+++ src/components/about-me.tsx
@@ -1,9 +1,14 @@
+import { cn } from "~/lib/utils";
+
 export default function AboutMe(
-  { children, imageUrl }: { children: React.ReactNode; imageUrl?: string },
+  { children, image }: {
+    children: React.ReactNode;
+    image?: { url?: string; object?: "contain" | "cover"; width?: number };
+  },
 ) {
   return (
-    <div className="flex justify-between h-full gap-16 flex-col lg:flex-row">
-      <div className="space-y-4">
+    <div className="flex justify-between h-full w-full gap-16 flex-col lg:flex-row">
+      <div className="space-y-4 flex-shrink">
         <h3>About me</h3>
 
         <div className="text-lg dark:text-white text-justify space-y-6 opacity-75">
@@ -11,12 +16,22 @@ export default function AboutMe(
         </div>
       </div>
 
-      {imageUrl && (
-        <div className="flex-grow max-w-sm lg:w-[1200px]">
+      {image && (
+        <div
+          style={{
+            maxWidth: image.width,
+          }}
+          className="flex-grow flex-shrink-0"
+        >
           <img
-            src={imageUrl}
+            src={image.url}
             alt="Profile picture"
-            className="rounded-lg object-cover w-full h-full"
+            className={cn(
+              "rounded-lg",
+              image.object === "contain"
+                ? "m-auto max-w-full min-h-0"
+                : "object-cover w-full h-full",
+            )}
           />
         </div>
       )}
